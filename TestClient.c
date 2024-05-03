48da481a134477929e1d1beac1417ad298e55b6e
chan1031 <dragonlsc@naver.com>
2024-05-03 15:37:19 +0900

    ver 2
    Update
    -error fix
    but it has multithread error

--- TestClient.c
+++ TestClient.c
@@ -7,17 +7,59 @@
 
 #define BUFSIZE 1254
 #define PORT 8888
-int main(){
+
+unsigned short calculateChecksum(FILE *file)
+{
+    fseek(file, 0, SEEK_SET);
+    unsigned short checksum = 0;
+    int ch;
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        checksum += (unsigned char)ch; 
+    }
+
+    fseek(file, 0, SEEK_SET);
+    return checksum;
+}
+
+void fileRecive(int sock, struct sockaddr_in *serv_addr, socklen_t adr_sz)
+{
+    FILE *file = fopen("received_image.jpg", "wb");
+    if (file == NULL)
+    {
+        perror("File opening failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char message[BUFSIZE];
+    size_t nbyte = BUFSIZE;
+    while (1)
+    {
+        nbyte = recvfrom(sock, message, nbyte, 0, (struct sockaddr *)serv_addr, &adr_sz);
+        if (!strcmp(message, "fin"))
+        {
+            printf("File received successfully.\n");
+            break;
+        }
+        fwrite(message, nbyte, 1, file);
+    }
+
+    fclose(file);
+}
+
+int main()
+{
     int sock;
     char message[BUFSIZE];
     int str_len;
     socklen_t adr_sz;
-    int count=0;
 
     struct sockaddr_in serv_addr, client_addr;
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sock == -1){
+    if (sock == -1)
+    {
         printf("socket() error\n");
         exit(1);
     }
@@ -28,34 +70,55 @@ int main(){
     serv_addr.sin_port = htons(PORT);
 
     char hello_message[] = "hello i am client";
-    sendto(sock, hello_message, strlen(hello_message), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    sendto(sock, hello_message, strlen(hello_message), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     adr_sz = sizeof(client_addr);
-    str_len=recvfrom(sock,message,BUFSIZE,0,(struct sockaddr*)&client_addr,&adr_sz);
-   
+    str_len = recvfrom(sock, message, BUFSIZE, 0, (struct sockaddr *)&client_addr, &adr_sz);
+
     message[str_len] = '\0';
     printf("Server say: %s\n", message);
 
-    //파일 전송     
-    FILE *file = fopen("received_test.mp4", "wb");
-    if (file == NULL) {
+    fileRecive(sock, &serv_addr, adr_sz);
+
+    FILE *file2;
+    char *filename = "received_image.jpg";
+    
+    if (file2 == NULL)
+    {
         perror("File opening failed");
         exit(EXIT_FAILURE);
     }
-
-    int nbyte = BUFSIZE;
-    while(1){
-        
-        nbyte = recvfrom(sock, message, BUFSIZE, 0, (struct sockaddr*)&serv_addr, &adr_sz);
-        count++;
-        if(!strcmp(message, "fin")){
-            printf("File received successfully count is %d\n",count); 
+    // checksum 검사
+    recvfrom(sock, message, BUFSIZE, 0, (struct sockaddr *)&client_addr, &adr_sz);
+    while (1)
+    {   
+        file2 = fopen(filename, "rb");
+        printf("received checksum = %s\n", message);
+        unsigned short checksum = calculateChecksum(file2);
+        printf("calcualte checksum = %d\n", checksum);
+        char checksumCheck[BUFSIZE];
+        sprintf(checksumCheck, "%d", checksum);
+        if (!strcmp(message, checksumCheck))
+        {   
+            printf("checksum correct\n");
+            sprintf(message, "success");
+            printf("%s\n", message);
+            message[7] = '\0';
+            sendto(sock, message, strlen(message), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+            fclose(file2);
             break;
         }
-        fwrite(message, BUFSIZE, 1, file);
+        else
+        {
+            printf("checksum incorrect");
+            printf("\n");
+            char retry_message[BUFSIZE] = "retry";
+            retry_message[6] = '\0';
+            sendto(sock, retry_message, strlen(retry_message), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+            fileRecive(sock, &serv_addr, adr_sz);
+        }
     }
 
-    fclose(file);
     close(sock);
-    
+
     return 0;
 }
--- TestServer.c
+++ TestServer.c
@@ -9,65 +9,114 @@
 
 #define PORT 8888
 #define BUF_SIZE 1254
-#define MAX_CLIENTS 10
-#define MAX_MESSAGE_SIZE 1024
+#define MAX_CLIENTS 100
 
-typedef struct {
-	struct sockaddr_in address;
-	int socket;
-	int id;
+typedef struct
+{
+    struct sockaddr_in address;
+    int socket;
+    int id;
 } Client;
+
 Client clients[MAX_CLIENTS];
 pthread_t threads[MAX_CLIENTS];
 int clientCount = 0;
 pthread_mutex_t mutex;
 
-void *handleClient(void *arg) {
-	int id = *((int *)arg);
-	char buffer[MAX_MESSAGE_SIZE];
-	ssize_t bytesRead;
-
-	while (1) {
-        char buf[BUF_SIZE];
-        ssize_t bytes_read;
-        // sending viedo file
-        printf("sending video file...\n");
-           size_t fsize;
-
-        //video file
-        FILE *file;
-        char *filename = "video.mp4";
-         // open video file
-        file = fopen(filename, "rb");
-        if (file == NULL) {
-            perror("File opening failed");
-            exit(EXIT_FAILURE);
-        }
-
-for (;;)   // loop for ever
-  {
-    char buffer[BUF_SIZE];
-    size_t bytesread = fread(buffer, 1, sizeof buffer, file);
-
-    // bytesread contains the number of bytes actually read
-    if (bytesread == 0)
-    {   
-        char fileFinMessage[] = "fin";
-        sendto(clients[id].socket, fileFinMessage, sizeof(fileFinMessage),0, (struct sockaddr*)&clients[id].address, sizeof(clients[id].address));
-      // no bytes read => end of file
-      break;
+unsigned short calculateChecksum(FILE *file)
+{
+    fseek(file, 0, SEEK_SET);
+    unsigned short checksum = 0;
+    int ch;
+    while ((ch = fgetc(file)) != EOF)
+    {
+        checksum += (unsigned char)ch; 
     }
 
-    // send the data
-    sendto(clients[id].socket, buffer, BUF_SIZE,0, (struct sockaddr*)&clients[id].address, sizeof(clients[id].address));
-  
-  } 
-printf("send video compelte");
-		
-}
+    fseek(file, 0, SEEK_SET);
+    return checksum;
 }
 
-int main(){
+void *handleClient(void *arg)
+{
+    int id = *((int *)arg);
+    ssize_t bytesRead;
+
+    // video file
+    FILE *file;
+    char *filename = "image.jpg";
+    // open video file
+    file = fopen(filename, "rb");
+    if (file == NULL)
+    {
+        perror("File opening failed");
+        exit(EXIT_FAILURE);
+    }
+    char buf[BUF_SIZE];
+    ssize_t bytes_read;
+    // check file size
+    fseek(file, 0, SEEK_END);
+    size_t file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // define loop count
+    size_t loopcount = file_size / BUF_SIZE;
+    // what is the residue
+    size_t residue = file_size % BUF_SIZE;
+    char buffer[BUF_SIZE];
+    int i=0;
+    pthread_mutex_lock(&mutex);
+    strcpy(buffer,"retry");
+    while (1)
+    {   
+        printf("received message: %s\n", buffer);
+        if (!strcmp(buffer, "success"))
+        {
+            break;
+        }
+        else if(!strcmp(buffer, "retry"))
+        {   printf("sending videofile\n");
+            // data sending
+            for (int i = 0; i < loopcount; i++)
+            {
+
+                size_t bytesread = fread(buffer, 1, BUF_SIZE, file);
+
+                sendto(clients[id].socket, buffer, BUF_SIZE, 0, (struct sockaddr *)&clients[id].address, sizeof(clients[id].address));
+            }
+
+            // residue data sending
+            size_t bytesread = fread(buffer, 1, residue, file);
+            sendto(clients[id].socket, buffer, residue, 0, (struct sockaddr *)&clients[id].address, sizeof(clients[id].address));
+
+            printf("sending video file completed\n");
+            char fileFinMessage[] = "fin";
+
+            if (sendto(clients[id].socket, fileFinMessage, sizeof(fileFinMessage), 0, (struct sockaddr *)&clients[id].address, sizeof(clients[id].address)) < 0)
+            {
+                perror("sendto() failed");
+                exit(EXIT_FAILURE);
+            }
+            // sending checksum
+            unsigned short checksum = calculateChecksum(file);
+            sprintf(buffer, "%d", checksum);
+            printf("checksum is %d \n", checksum);
+            sendto(clients[id].socket, buffer, BUF_SIZE, 0, (struct sockaddr *)&clients[id].address, sizeof(clients[id].address));
+
+        }
+        //is data transfer has not error?
+        bytes_read=recvfrom(clients[id].socket, buffer, sizeof(buffer), 0, NULL, NULL);
+        buffer[bytes_read]='\0';
+        
+    }
+
+    fclose(file);
+    pthread_mutex_lock(&mutex);
+    return 0;
+}
+
+int main()
+{
     int serv_sock;
     struct sockaddr_in serv_adr;
     char message[BUF_SIZE];
@@ -76,58 +125,63 @@ int main(){
     ssize_t str_len;
 
     // Create socket
-	if ((serv_sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-		perror("Socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+    if ((serv_sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    //socket address
+    // socket address
     memset(&serv_adr, 0, sizeof(serv_adr));
-    serv_adr.sin_family=AF_INET;
-    serv_adr.sin_addr.s_addr=INADDR_ANY;
-    serv_adr.sin_port=htons(PORT);
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = INADDR_ANY;
+    serv_adr.sin_port = htons(PORT);
 
-    //binding socket
-    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1){
+    // binding socket
+    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
+    {
         perror("bind() error");
         exit(1);
     }
 
-    //mutex Init
+    // mutex Init
     pthread_mutex_init(&mutex, NULL);
 
     printf("waiting for client...\n");
 
-    //Create a thraed for each client
-    while(1){
-
-        //receive message from client
-        str_len=recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr *)&clients[clientCount].address, &clnt_adr_sz);
-         if (str_len < 0) {
+    // Create a thraed for each client
+    while (1)
+    {
+        // receive message from client
+        str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr *)&clients[clientCount].address, &clnt_adr_sz);
+        if (str_len < 0)
+        {
             perror("recvfrom error");
-            exit(1);
-        }  
-
-        //print message
-        message[str_len] = '\0';
-        printf("client %d say: %s\n", clientCount+1, message);
-
-        //Add client to the list
-        clients[clientCount].socket = serv_sock;
-        clients[clientCount].id = clientCount;
-        //create thread for client
-        pthread_create(&threads[clientCount], NULL, handleClient, (void *)&clients[clientCount].id);
-        clientCount++;
-        //send a message to the client
-        sprintf(message,"hello i am server, you are Client %d\n", clientCount);
-        if (sendto(clients[clientCount-1].socket, message, strlen(message), 0, (struct sockaddr*)&clients[clientCount-1].address, sizeof(clients[clientCount - 1].address))< 0) {
-            perror("sendto error");
             exit(1);
         }
 
+        // print message
+        message[str_len] = '\0';
+        printf("client %d say: %s\n", clientCount + 1, message);
+
+        // Add client to the list
+        clients[clientCount].socket = serv_sock;
+        clients[clientCount].id = clientCount;
+        
+        clientCount++;
+        // send a message to the client
+        sprintf(message, "hello i am server, you are Client %d\n", clientCount);
+        if (sendto(clients[clientCount - 1].socket, message, strlen(message), 0, (struct sockaddr *)&clients[clientCount - 1].address, sizeof(clients[clientCount - 1].address)) < 0)
+        {
+            perror("sendto error");
+            exit(1);
+        }
+        // create thread for client
+        pthread_create(&threads[clientCount+1], NULL, handleClient, (void *)&clients[clientCount+1].id);
+        pthread_join(threads[clientCount+1], NULL);
     }
+
     close(serv_sock);
     pthread_mutex_destroy(&mutex);
     return 0;
-    
 }
